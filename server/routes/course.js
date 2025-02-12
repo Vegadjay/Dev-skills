29b42af86516b45447c648331d98fe9dade5dcbf
renil <renilgarala360004@gmail.com>
2025-02-12 15:41:53 +0530

    Razorpay Payment Gateway

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllCourses, getSingleCourse, fetchLectures, fetchLecture, getMyCourses } from '../controllers/course.js';
+import { getAllCourses, getSingleCourse, fetchLectures, fetchLecture, getMyCourses, checkout, paymentVerification } from '../controllers/course.js';
 import {isAuth} from '../middlewares/isAuth.js';
 
 
@@ -10,5 +10,7 @@ router.get("/course/:id", getSingleCourse);
 router.get("/lectures/:id", isAuth, fetchLectures);
 router.get("/lecture/:id", isAuth, fetchLecture);
 router.get("/mycourse", isAuth, getMyCourses);
+router.post("/course/checkout/:id", isAuth, checkout);
+router.post("/verification/:id", isAuth, paymentVerification);
 
 export default router;
