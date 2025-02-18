8d9021a2415ad26cad52634913d12ee78d320410
renil <renilgarala360004@gmail.com>
2025-02-18 21:03:02 +0530

    course detail page and course page with user and admin access

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,21 +8,41 @@ import Register from "./pages/auth/Register";
 import Verify from "./pages/auth/Verify";
 import Footer from "./components/footer/Footer";
 import About from "./pages/about/About";
+import Account from "./pages/account/Account";
+import { UserData } from "./context/UserContext";
+import Loading from "./components/loading/Loading";
+import Courses from "./pages/courses/courses";
+import CourseDescription from "./pages/coursedescription/CourseDescription";
 
 const App = () => {
+  const { isAuth, user, loading } = UserData();
   return (
     <>
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/verify" element={<Verify />} />
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
+      {loading ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          <Header isAuth={isAuth} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/courses" element={<Courses />} />
+
+            <Route
+              path="/account"
+              element={isAuth ? <Account user={user} /> : <Login />}
+            />
+            <Route path="/login" element={isAuth ? <Home /> : <Login />} />
+            <Route
+              path="/register"
+              element={isAuth ? <Home /> : <Register />}
+            />
+            <Route path="/verify" element={isAuth ? <Home /> : <Verify />} />
+            <Route path="/course/:id" element={isAuth ? <CourseDescription/> : <Login/>}/>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      )}
     </>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { UserContextProvider } from './context/UserContext.jsx'
+import { CourseContextProvider } from './context/CourseContext.jsx';
+
+
+export const server = "http://localhost:5001";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <UserContextProvider>
+      <CourseContextProvider>
+        <App />
+      </CourseContextProvider>
+    </UserContextProvider>
   </StrictMode>,
 )
